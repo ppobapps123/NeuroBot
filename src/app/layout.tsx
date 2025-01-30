6edbc71c7ebc603cb3a5ddd284d2875455255a35
zayyid123 <mochamadzayyid@gmail.com>
2025-01-30 16:37:36 +0700

    add SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,35 @@ export default function RootLayout({
           name="format-detection"
           content="telephone=no, date=no, email=no, address=no"
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window._ow = window._ow || {};
+  window.__ow.organizationId = "e59959fd-b9d4-44d7-80e7-5876dcd30cb2";
+  window.__ow.template_id = "25196731-6890-4414-9545-c17bed9d907c";
+  window.__ow.integration_name = "manual_settings";
+  window.__ow.product_name = "chatbot";   
+  ;(function(n,t,c){function i(n){return e.h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[OpenWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.openwidget.com/openwidget.js",t.head.appendChild(n)}};!n._ow.asyncInit&&e.init(),n.OpenWidget=n.OpenWidget||e}(window,document,[].slice))
+          `,
+          }}
+        />
+        <noscript>
+          You need to{" "}
+          <a
+            href="https://www.chatbot.com/help/chat-widget/enable-javascript-in-your-browser/"
+            rel="noopener nofollow"
+          >
+            enable JavaScript
+          </a>{" "}
+          in order to use the AI chatbot tool powered by{" "}
+          <a
+            href="https://www.chatbot.com/"
+            rel="noopener nofollow"
+            target="_blank"
+          >
+            ChatBot
+          </a>
+        </noscript>
       </head>
       <body suppressHydrationWarning={true} dir="ltr">
         <AppContextProvider>
